2071676800103705429621e301417ad1153d87e6
Aboulfotouh <144533464+AbdelrahmanAboulfotouh@users.noreply.github.com>
2024-01-12 14:35:50 +0200

    Create 131 A. cAPS lOCK.cpp

---
+++ Codeforces/Div2/131 A. cAPS lOCK.cpp
@@ -0,0 +1,31 @@
+//https://codeforces.com/problemset/problem/131/A
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<string>
+#include<map>
+#define lop(n) for (int i = 0; i < n; i++)
+using namespace std;
+int main()
+    {
+       string str; cin>>str;
+       bool f=false;
+        int u{0};
+       for(int i{0};i<str.size();++i)
+                if(isupper(str[i]))
+                    ++u;
+            if(u==str.size()||(u==str.size()-1&&islower(str[0])))
+            f=true;
+            if(f)
+            {
+                for(int i{0};i<str.size();++i)
+                if(isupper(str[i]))
+                    str[i]=(tolower(str[i]));
+                else
+                    str[i]=(toupper(str[i]));
+
+            }
+             cout<<str;
+
+    return 0;
+    }
