a3222335f8016301fd9fba6605a7f3a20d907a3a
Aboulfotouh <144533464+AbdelrahmanAboulfotouh@users.noreply.github.com>
2024-01-13 19:28:41 +0200

    Create 50 A. Domino piling.cpp

---
+++ Codeforces/Div3/50 A. Domino piling.cpp
@@ -0,0 +1,12 @@
+//https://codeforces.com/contest/50/problem/A
+#include<iostream>
+using namespace std;
+
+int main()
+    {
+       int m,n;
+       cin>>m>>n;
+       int ans=(m*n)/2;
+        cout<<ans<<'\n';
+    return 0;
+    }
