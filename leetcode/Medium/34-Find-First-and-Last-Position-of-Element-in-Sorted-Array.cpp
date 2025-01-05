6f594fbf805bc1bb53c14ba7b0919986259d6508
Aboulfotouh <144533464+AbdelrahmanAboulfotouh@users.noreply.github.com>
2025-01-05 13:09:19 +0200

    [34] [Time Beats: 100.00%] [Memory Beats: 12.88%] - LeetPush

---
+++ leetcode/Medium/34-Find-First-and-Last-Position-of-Element-in-Sorted-Array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        auto s=lower_bound(nums.begin(), nums.end(), target);
+        auto e=upper_bound(nums.begin(), nums.end(), target);
+
+        if(s == e)
+            return {-1,-1};
+    
+    return {(int)(s-nums.begin()),(int) (e-nums.begin()-1)};
+
+    }
+};
