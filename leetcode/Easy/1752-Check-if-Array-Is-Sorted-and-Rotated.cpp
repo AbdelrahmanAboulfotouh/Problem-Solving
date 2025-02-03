eca7178b87b1c593eb3d7845ac2dc4d9d92400ea
Aboulfotouh <144533464+AbdelrahmanAboulfotouh@users.noreply.github.com>
2025-02-04 01:16:54 +0200

    [1752] [Time Beats: 100.00%] [Memory Beats: 24.62%] - LeetPush

--- leetcode/Easy/1752-Check-if-Array-Is-Sorted-and-Rotated.cpp
+++ leetcode/Easy/1752-Check-if-Array-Is-Sorted-and-Rotated.cpp
@@ -1,37 +1,14 @@
 class Solution {
 public:
-    bool check(vector<int>& nums) {
+    bool check(vector<int>& nums) 
+    {
         int n = nums.size();
-
-        // Construct the rotated array
-        vector<int> checkSorted(n);
-
-        // Iterate through all possible rotation offsets
-        for (int rotationOffset = 0; rotationOffset < n; ++rotationOffset) {
-            int currIndex = 0;
-            for (int index = rotationOffset; index < n; ++index) {
-                checkSorted[currIndex++] = (nums[index]);
-            }
-            for (int index = 0; index < rotationOffset; ++index) {
-                checkSorted[currIndex++] = (nums[index]);
-            }
-
-            // Check if the constructed array is sorted
-            bool isSorted = true;
-            for (int index = 0; index < n - 1; ++index) {
-                if (checkSorted[index] > checkSorted[index + 1]) {
-                    isSorted = false;
-                    break;
-                }
-            }
-
-            // If sorted, return true
-            if (isSorted) {
-                return true;
-            }
+        int Inversions = (int)(nums[0] < nums[n-1]);
+        for(int i{1};i<n;++i)
+        {
+            if(nums[i] < nums[i-1])
+                ++Inversions;
         }
-
-        // If no rotation makes the array sorted, return false
-        return false;
+    return  Inversions <=1 ;
     }
 };
