d8cd4389476d86c3ad5e796c089d4789a578ff59
AbdelrahmanAboulfotouh <144533464+AbdelrahmanAboulfotouh@users.noreply.github.com>
2025-09-07 04:58:04 +0300

    [1268] [Time Beats: 35.95%] [Memory Beats: 43.45%] - LeetPush

--- leetcode/Medium/Search-Suggestions-System.cpp
+++ leetcode/Medium/Search-Suggestions-System.cpp
@@ -1,30 +1,69 @@
-class Solution {
+class Trie {
+    vector<Trie*>childs;
+    bool is_leaf;
 public:
-    vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
-        unordered_map<string, vector<string>>mp;
-        sort(products.begin(),products.end());
-        string sub;
-        for(int i{0};i<searchWord.size();++i)
+    Trie():childs(26, nullptr),is_leaf(false) {}
+    void insert(const string &word,int idx = 0)
+    {
+        if(idx == word.size())
+            is_leaf = true;
+        else
         {
-             sub += searchWord[i];
-            for(int j{0}, limit{0} ; limit < 3 and j<products.size();++j)
+            int cur = word[idx] - 'a';
+            if (!childs[cur])
+                childs[cur] = new Trie();
+            childs[cur]->insert(word, idx + 1);
+        }
+    }
+    void get_all_words(Trie* cur , string &word,vector<string> &result )
+    {
+        if(result.size() == 3)
+            return;
+        if(cur->is_leaf)
+            result.push_back(word);
+        for(int i{0};i<26;++i)
+        {
+            if(cur->childs[i])
             {
-                auto pos = products[j].rfind(sub,0);
-                if(pos == 0)
-                {
-                    mp[sub].push_back(products[j]);
-                    ++limit;
-                }
+                word+=(char)('a' + i);
+                get_all_words(cur->childs[i],word,result);
+                word.pop_back();
             }
         }
-        sub.clear();
-         vector<vector<string>> ans;
-        for(int i{0};i<searchWord.size();++i)
-        {
-             sub +=  searchWord[i];
-            ans.push_back(mp[sub]);
-        }
 
-        return ans;
     }
+    vector<string> get_words_with_prefix(string prefix,vector<string> result)
+    {
+        Trie* curr = this;
+        for(int i{0};i<prefix.size();++i)
+        {
+            int cur = prefix[i] - 'a';
+            if(!curr->childs[cur])
+                return result;
+            curr = curr->childs[cur];
+        }
+        get_all_words(curr,prefix,result);
+        return result;
+    }
+
+};
+class Solution {
+    
+public:
+ vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
+        sort(products.begin(),products.end());
+       Trie trie;
+       for(auto &word: products)
+                trie.insert(word);
+        
+   vector<vector<string>>ans;
+   string sub;
+   for(auto &c: searchWord)
+   {
+    sub+=c;
+    vector<string>sub_result;
+    ans.push_back(trie.get_words_with_prefix(sub,sub_result));
+   }
+   return ans;
+ }
 };
