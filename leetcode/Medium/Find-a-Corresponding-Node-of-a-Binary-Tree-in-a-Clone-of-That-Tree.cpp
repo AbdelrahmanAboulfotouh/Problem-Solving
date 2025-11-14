83737fd672b3a1a3fa481477e808eda5faf3afdb
AbdelrahmanAboulfotouh <144533464+AbdelrahmanAboulfotouh@users.noreply.github.com>
2025-11-14 16:34:38 +0200

    [1379] [Time Beats: 76.96%] [Memory Beats: 27.16%] - LeetPush

---
+++ leetcode/Medium/Find-a-Corresponding-Node-of-a-Binary-Tree-in-a-Clone-of-That-Tree.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    TreeNode* getTargetCopy(TreeNode* original, TreeNode* cloned, TreeNode* target) {
+        if(!cloned)
+            return nullptr;
+        if(target->val == cloned->val)
+            return cloned;
+        TreeNode* ans = nullptr;
+        ans =  getTargetCopy( original->left,cloned->left,target);
+        if(!ans)
+            ans =  getTargetCopy( original->right,cloned->right,target);
+    return ans;
+    }
+};
