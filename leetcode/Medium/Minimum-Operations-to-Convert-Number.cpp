2440e9ca2fb70450e5d54ee4429d1d542b33dbed
AbdelrahmanAboulfotouh <144533464+AbdelrahmanAboulfotouh@users.noreply.github.com>
2025-12-12 00:23:55 +0200

    [2059] [Time Beats: 67.30%] [Memory Beats: 98.11%] - LeetPush

--- leetcode/Medium/Minimum-Operations-to-Convert-Number.cpp
+++ leetcode/Medium/Minimum-Operations-to-Convert-Number.cpp
@@ -5,7 +5,7 @@
 5
 6int  bfs(vector<int>& nums,  int start, int goal)
 7{
-8    map<int, bool> visited;
+8   vector<bool> visited(1001,false);
 9    queue<int> q;
 10    q.push(start);
 11    visited[start] = true;
@@ -24,19 +24,19 @@
 24                        int nextVal = curVal + nums[i];
 25                        if (nextVal == goal)
 26                                return level + 1;
-27                        if(nextVal >=0 && nextVal <= 1000 && visited.find(nextVal) == visited.end())
+27                        if(nextVal >=0 && nextVal <= 1000 && !visited[nextVal])
 28                          {  q.push(nextVal);
 29                            visited[nextVal] = true;}
 30                        nextVal = curVal - nums[i];
 31                         if (nextVal == goal)
 32                                return level + 1;
-33                          if(nextVal >=0 && nextVal <= 1000 && visited.find(nextVal) == visited.end())
+33                        if(nextVal >=0 && nextVal <= 1000 && !visited[nextVal])
 34                          {  q.push(nextVal);
 35                            visited[nextVal] = true;}
 36                        nextVal = curVal ^ nums[i];
 37                         if (nextVal == goal)
 38                                return level + 1;
-39                          if(nextVal >=0 && nextVal <= 1000 && visited.find(nextVal) == visited.end())
+39                         if(nextVal >=0 && nextVal <= 1000 && !visited[nextVal])
 40                          {  q.push(nextVal);
 41                            visited[nextVal] = true;}
 42                    }
