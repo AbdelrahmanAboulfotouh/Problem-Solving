f5dbe9c31c4ba1243425aa1ff0ecf32d09ed0f91
AbdelrahmanAboulfotouh <144533464+AbdelrahmanAboulfotouh@users.noreply.github.com>
2026-03-31 08:44:11 +0200

    [475] [Time Beats: 64.61%] [Memory Beats: 11.09%] - LeetPush

--- leetcode/Medium/Heaters.cpp
+++ leetcode/Medium/Heaters.cpp
@@ -1,41 +1,32 @@
-1class Solution 
-2{
-3public:
-4bool isValid(vector<int>& houses, vector<int>& heaters, int raduis)
-5{
-6    int covered{0};int idx{0};
-7    int Hsz = houses.size();
-8    for(auto &h : heaters)
-9    {
-10        int left = h - raduis;
-11        int right = h + raduis;
-12        while(idx < Hsz && left <=houses[idx] && right>=houses[idx])
-13        {
-14            ++idx;
-15            ++covered;
-16        }
-17    }
-18    return (covered == Hsz);
-19}
-20    int findRadius(vector<int>& houses, vector<int>& heaters) 
-21    {
-22       sort(houses.begin(),houses.end());
-23       sort(heaters.begin(),heaters.end());
-24       int ans{0};
-25       int l = 0 ; 
-26       int r = max(houses[houses.size()-1], heaters[heaters.size()-1]);
-27       while(l<=r)
-28       {
-29            int radius = l + (r-l)/2;
-30            if(isValid(houses,heaters,radius))
-31            {
-32                ans = radius;
-33                r = radius-1;
-34            }
-35            else
-36             l = radius+1;
-37
-38       }
-39       return ans;
-40    }
-41};
+1class Solution {
+2public:
+3    int findRadius(vector<int>& houses, vector<int>& heaters) {
+4        vector<int>minDist(houses.size());
+5        sort(heaters.begin(),heaters.end());
+6        int ans{0};
+7
+8        for(int i{0};i<houses.size();++i)
+9        {
+10            int idx = lower_bound(heaters.begin(),heaters.end(),houses[i]) - heaters.begin();
+11            if(idx == heaters.size())
+12            {
+13                     ans = max(ans,abs(heaters[idx-1] - houses[i]));
+14            }
+15            else if(idx == 0)
+16            {
+17                ans = max(ans,abs(heaters[idx] - houses[i]));
+18
+19            }
+20            else
+21            {
+22                int Min = min(abs(heaters[idx] - houses[i]), abs(heaters[idx-1] - houses[i]));
+23                ans = max(ans,Min);
+24            }
+25
+26           
+27        }
+28
+29        
+30        return ans;
+31    }
+32};
